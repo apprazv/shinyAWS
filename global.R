4b9850ef7b53a9960f52b6e6874e35dfe5b74239
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2021-06-13 10:13:21 -0600

    enabling csv read into globalvars

--- global.R
+++ global.R
@@ -4,15 +4,18 @@ library(DT)
 library(shinyWidgets)
 library(shinymanager)
 library(aws.s3)
+library(shinydashboard)
 
-# aws_s3_creds <- read.csv("api_keys/new_user_credentials.csv")
-# Sys.setenv(
-#   "AWS_STORAGE_BUCKET_NAME" = 'ipractise',
-#   "AWS_ACCESS_KEY_ID" = aws_s3_creds$Access.key.ID,
-#   "AWS_SECRET_ACCESS_KEY" = aws_s3_creds$Secret.access.key,
-#   "AWS_S3_CUSTOM_DOMAIN" = "'%s.s3.amazonaws.com' % ipractise",
-#   "AWS_S3_REGION_NAME" = 'us-east-1'
-# )
+#### Insert the AWS CSV Output in the api_keys folder of directory ####
+
+aws_s3_creds <- read.csv("api_keys/new_user_credentials.csv")
+Sys.setenv(
+  "AWS_STORAGE_BUCKET_NAME" = 'bucket_name',
+  "AWS_ACCESS_KEY_ID" = aws_s3_creds$Access.key.ID,
+  "AWS_SECRET_ACCESS_KEY" = aws_s3_creds$Secret.access.key,
+  "AWS_S3_CUSTOM_DOMAIN" = "'%s.s3.amazonaws.com' % ipractise",
+  "AWS_S3_REGION_NAME" = 'us-east-1'
+)
 
 
 # Making a HexSticker #
